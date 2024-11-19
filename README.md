b77428cf770224fefd7fdc9463ee8669412ce7aa
Musooo <nicoloblack2@gmail.com>
2024-11-19 17:16:06 +0100

    assign values of base,exp and mod using argv

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # idempotence
+build:
+mingw32-make (on windows)
+make (other)
+run: ./executable base exponent module
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "decimalToBin.h"
 
 int moduleExp(int a, int m, int n) {
@@ -10,22 +11,22 @@ int moduleExp(int a, int m, int n) {
 }
 
 int main(int argc, char *argv[]) {
-    int a = 22; // base
-    int m = 8; // modulo
-    int n = 5; // esponente
+    int a = atoi(argv[1]); // base
+    int n = atoi(argv[2]); // esponente
+    int m = atoi(argv[3]); // modulo
 
     int res = moduleExp(a, m, n);
     printf("%d\n", res);
 
-    int *arr = NULL;
-    int size = 0;
-    dToB(&arr, a, &size);
+    // int *arr = NULL;
+    // int size = 0;
+    // dToB(&arr, a, &size);
 
-    for (int i = size - 1; i >= 0; i--) {
-        printf("%d", arr[i]);
-    }
-    printf("\n");
+    // for (int i = size - 1; i >= 0; i--) {
+    //     printf("%d", arr[i]);
+    // }
+    // printf("\n");
 
-    free(arr);
+    // free(arr);
     return 0;
 }
